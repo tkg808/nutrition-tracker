62dca18b67637aba6a5c969f0a1376fb11ae75d1
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-03-03 16:09:29 -0800

    Able to render the context array in Home.js.

---
+++ src/components/FoodList.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+export default function FoodList({ userFoods, handleRemove })
+{
+  return (
+    <div className="nutrition-list-container">
+      <h3>List of Foods</h3>
+      {
+        userFoods.length ?
+          <ul className="list-container">
+            {userFoods.map((food, index) =>
+            {
+              return (
+                <li className="food-item" id={index}>
+                  <h2> {food.name.toUpperCase()}</h2>
+                  <p>Serving Size: {food.serving_size_g}g</p>
+                  <h3>Calories: {food.calories}</h3>
+                  <h4>Fats: {food.fat_total_g}g</h4>
+                  <p>Saturated Fats: {food.fat_saturated_g}g</p>
+                  <h4>Carbs: {food.carbohydrates_total_g}g</h4>
+                  <p>Sugar: {food.sugar_g}g</p>
+                  <p>Fiber: {food.fiber_g}g</p>
+                  <h4>Protein: {food.protein_g}g</h4>
+                  <button id="remove-food" onClick={handleRemove}>Remove</button>
+                </li>
+              )
+            })}
+          </ul> :
+          <>
+            <img
+              src="https://media.giphy.com/media/WsMOkoJpQEgF0HHLfH/giphy.gif"
+              className="empty-list">
+
+            </img>
+            <p>Try adding some foods to your list...</p>
+          </>
+      }
+    </div>
+  );
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NutritionTotal from './NutritionTotal';
-import NutritionList from './NutritionList';
+import FoodList from './FoodList';
+import { UserContext } from '../UserContext';
 
 export default function Home()
 {
+  const { userFoods, setUserFoods } = useContext(UserContext);
+
+  function handleRemove(event)
+  {
+    console.log(event.target);
+  }
+
   return (
     <div>
       <NutritionTotal />
-      <NutritionList />
+      <FoodList
+        userFoods={userFoods}
+        handleRemove={handleRemove} />
     </div>
   )
 }
