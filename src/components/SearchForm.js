18e6ee21c92376fcbd87a874a437ba0861ea36e6
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-05-15 18:08:55 -0700

    Make textarea's size responsive.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,20 +3,18 @@ import React from 'react';
 export default function SearchForm({ searchInput, handleChange, handleSubmit })
 {
   return (
-    <div className="search-form">
-      <form onSubmit={handleSubmit}>
+    <>
+      <form onSubmit={handleSubmit} className="search-form">
         <label htmlFor="search-input"></label>
         <textarea
           type="text"
           id="search-input"
           value={searchInput}
           onChange={handleChange}
-          rows="5"
-          cols="40"
           placeholder="Example: 8oz steak, 2 eggs, and rice"
           required />
         <button type="submit">Search</button>
       </form>
-    </div>
+    </>
   )
 }
