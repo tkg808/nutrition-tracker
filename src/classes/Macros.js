b1bd71fec0278032c9d215791a53dbb445b23f29
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2022-05-29 18:39:49 -0700

    Update class methods and move to classes folder.

--- src/classes/Macros.js
+++ src/classes/Macros.js
@@ -14,18 +14,18 @@ export default class Macros
   getFats(calories)
   {
     const fats = calories * (this.fatsPercent / 100) / 9;
-    return (fats ? fats.toFixed(1) : "~");
+    return fats.toFixed(1);
   }
 
   getCarbs(calories)
   {
     const carbs = calories * (this.carbsPercent / 100) / 4;
-    return (carbs ? carbs.toFixed(1) : "~");
+    return carbs.toFixed(1);
   }
 
   getProteins(calories)
   {
     const proteins = calories * (this.proteinsPercent / 100) / 4;
-    return (proteins ? proteins.toFixed(1) : "~");
+    return proteins.toFixed(1);
   }
 }
